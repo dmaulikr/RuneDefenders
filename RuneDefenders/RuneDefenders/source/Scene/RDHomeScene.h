4f607cb2299a057c0a13bd948fce6423a909c5f5
Tekka <tekkaman@foxmail.com>
2014-04-09 13:46:09 +0800

    1. fix error of too large atlas png 2. add RDHomeScene.h/.m

---
+++ RuneDefenders/RuneDefenders/source/Scene/RDHomeScene.h
@@ -0,0 +1,13 @@
+//
+//  RDHomeScene.h
+//  RuneDefenders
+//
+//  Created by Tekka on 4/9/14.
+//  Copyright (c) 2014 TekkaInc. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface RDHomeScene : SKScene
+
+@end
---
+++ RuneDefenders/RuneDefenders/source/Scene/RDHomeScene.m
@@ -0,0 +1,13 @@
+//
+//  RDHomeScene.m
+//  RuneDefenders
+//
+//  Created by Tekka on 4/9/14.
+//  Copyright (c) 2014 TekkaInc. All rights reserved.
+//
+
+#import "RDHomeScene.h"
+
+@implementation RDHomeScene
+
+@end
